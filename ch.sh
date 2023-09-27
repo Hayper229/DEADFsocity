2aacf2beade5c76af2c8d8df85f7e833b679c36a
Hayper229 <105974108+Hayper229@users.noreply.github.com>
2023-09-27 17:39:43 +0300

    Add files via upload

---
+++ ch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo 1
+sleep 1
+echo 2
+sleep 1
+echo 3
+echo "Starting."
+echo "Starting.."
+echo "Starting..."
+
+
+echo "[INFO] We grant launch rights, please wait"  
+chmod +x DEADFsocity.py
+chmod +x DorkingScriptConsole.py
+chmod +x DosFM.py
+chmod +x Rdos.py
+chmod +x TCPflood.py
+chmod +x UDPDosser.py
+chmod +x dnsinfo.py
+chmod +x install_modules.py 
+chmod +x pars.py
+chmod +x parser.php
+chmod +x ph.py
+chmod +x scanner.py
+chmod +x sniff.py 
+chmod +x whois_info.py
+echo  "[OK] Finish"
